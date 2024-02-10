bf80c8457f7c953234d294aefaec1efb8fef62bd
IDK <79144483+aufarakha@users.noreply.github.com>
2024-02-10 17:38:26 +0700

    Update decrypt_thermal.sh

--- decrypt_thermal.sh
+++ decrypt_thermal.sh
@@ -3,7 +3,6 @@ LIST=$(ls my_thermal/*.conf)
 mkdir output_dec
 
 for FILE in $LIST; do
-    FILE_NAME=$(echo "$FILE" | sed 's/\.conf$//' | echo "_dec.conf")
     
     ./mi-thermal-crypt -i $FILE -o $FILE
     mv $FILE output_dec
